e5aef088fb5aed72b20612add56440b0d845a0a7
tinyzimmer <38474291+tinyzimmer@users.noreply.github.com>
2020-07-28 21:11:05 +0300

    cleanup api godoc

--- pkg/apis/kvdi/v1alpha1/register.go
+++ pkg/apis/kvdi/v1alpha1/register.go
@@ -1,8 +1,3 @@
-// NOTE: Boilerplate only.  Ignore this file.
-
-// Package v1alpha1 contains API Schema definitions for the kvdi v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=kvdi.io
 package v1alpha1
 
 import (
--- pkg/apis/meta/v1/register.go
+++ pkg/apis/meta/v1/register.go
@@ -1,8 +1,3 @@
-// NOTE: Boilerplate only.  Ignore this file.
-
-// Package v1 contains API Schema definitions for the meta v1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=kvdi.io
 package v1
 
 import (
