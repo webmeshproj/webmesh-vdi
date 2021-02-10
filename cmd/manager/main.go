3e09f703a7f2166d0f7ddf9c2f843cdcc94cd19d
Avi Zimmerman <avi.zimmerman@gmail.com>
2021-02-10 09:29:50 +0200

    fixed bug in creating prometheus instances from kubebuilder migration

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -29,6 +29,7 @@ import (
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -57,6 +58,7 @@ func init() {
 	utilruntime.Must(desktopsv1.AddToScheme(scheme))
 	utilruntime.Must(rbacv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
+	utilruntime.Must(monitoringv1.AddToScheme(scheme))
 }
 
 func main() {
